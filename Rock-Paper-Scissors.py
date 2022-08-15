d9906b43bdd5fd65cc738cc465083397e501717a
ushasree513 <ushasree513@gmail.com>
2022-08-15 18:37:49 +0530

    basic version

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,24 @@
+import random
+from tkinter import *
+from PIL import ImageTk,Image
+
+root= Tk()
+root.geometry("800x500")
+paper = ImageTk.PhotoImage(Image.open('paper.PNG'))
+stone = ImageTk.PhotoImage(Image.open('stone.PNG'))
+scissors = ImageTk.PhotoImage(Image.open('scissors.PNG'))
+label1 = Label(root)
+
+def logic():
+    Output = random.randint(1,3)
+    if(Output == 1):
+        label1.config(image=paper)
+        label1.pack()
+    elif(Output == 2):
+        label1.config(image=stone)
+        label1.pack()
+    else:
+        label1.config(image=scissors)
+        label1.pack()
+b1 = Button(root,text="Lets Play Rock-Paper-Scissors", font=("Bold",30),command=logic).pack()
+root.mainloop()
