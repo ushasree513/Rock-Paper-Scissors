4078787e8d78dac9651b06ba7ab68c44ddbfc9ae
ushasree513 <ushasree513@gmail.com>
2022-08-16 20:22:33 +0530

    Included Individual User Score

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,40 +1,50 @@
 import random
 from tkinter import *
-from unittest import result
 from PIL import ImageTk,Image
 
 root= Tk()
 root.geometry("800x500")
+
 paper = ImageTk.PhotoImage(Image.open('paper.PNG'))
 stone = ImageTk.PhotoImage(Image.open('stone.PNG'))
 scissors = ImageTk.PhotoImage(Image.open('scissors.PNG'))
 label1 = Label(root)
-Result = Button(root,font=("Bold",30))
-
+Result1 = Label(root,font=("Bold",25))
+Result2 = Label(root,font=("Bold",25))
+Result3 = Label(root,font=("Bold",25))
+sum1 = 0
+sum2 = 0
+sum3 = 0
 
 
 
 def logic():
     Output = random.randint(1,3)
-    sum1 = 0
+    global sum1
     if(Output == 1):
         label1.config(image=paper)
         label1.place(x=0,y=150)
         sum1 = sum1 + 1
+        
+        
     elif(Output == 2):
         label1.config(image=stone)
         label1.place(x=0,y=150)
         sum1 = sum1 + 2
+        
+        
     else:
         label1.config(image=scissors)
         label1.place(x=0,y=150)
-        sum1 + sum1 + 3
-    Result.config(text=sum1)
-    Result.place(x=500,y=0)
+        sum1 = sum1 + 3
+    Result1.config(text="Score : " +str(sum1),bg="Light Pink")
+    Result1.place(x=120,y=85) 
+    
+       
         
 def logic2():
     Output = random.randint(1,3)
-    sum2 = 0
+    global sum2
     
     if(Output == 1):
         label1.config(image=paper)
@@ -48,12 +58,12 @@ def logic2():
         label1.config(image=scissors)
         label1.place(x=500,y=150)
         sum2 = sum2 + 3
-    Result.config(text=sum2)
-    Result.place(x=0,y=0)
+    Result2.config(text="Score : " +str(sum2),bg="Light Pink")
+    Result2.place(x=620,y=85)
 
 def logic3():
     Output = random.randint(1,3)
-    sum3 = 0
+    global sum3
     if(Output == 1):
         label1.config(image=paper)
         label1.place(x=1000,y=150)
@@ -66,8 +76,8 @@ def logic3():
         label1.config(image=scissors)
         label1.place(x=1000,y=150)
         sum3 = sum3 + 3
-    Result.config(text=sum3)
-    Result.place(x=0,y=0)
+    Result3.config(text="Score : " + str(sum3),bg="Light Pink")
+    Result3.place(x=1120,y=85)
 label2 = Button(root,text="Lets Play Rock-Paper-Scissors", font=("Bold",30),bg="Light Blue")
 label2.place(height=50)
 label2.pack(fill=X)
